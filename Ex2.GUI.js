3be21b8f4c6622e9fc8dc7ffc3cbd5f0a42105f9
aditayri <aditayri@gmail.com>
2017-12-17 12:24:17 +0200

    Add files via upload

---
+++ Ex2.GUI.js
@@ -0,0 +1,129 @@
+Ex2 = window.Ex2 || {};
+
+Ex2.GUI = function()
+{
+  let canvas;
+    let w, h;
+let initModule = function()
+{
+	 this.canvas = document.getElementById("canvas");
+		this.canvas.width = window.innerWidth -30
+		this.canvas.height = window.innerHeight-30 ;
+        w = 600;
+        h = 600;
+        Ex2.Logic.initModule();    
+        drawBoard();
+		document.onkeydown = keyHandler.bind(this);
+				
+
+};
+
+function keyHandler(event)
+{
+		var e = event || window.event;
+		if(e.keyCode == '37')
+			Ex2.Logic.move('left');
+		else if(e.keyCode == '38')
+			Ex2.Logic.move('up');
+		else if(e.keyCode == '39')
+			Ex2.Logic.move('right');
+		else if(e.keyCode == '40')
+			Ex2.Logic.move('down');
+		else if(e.keyCode == '67')
+			Ex2.Logic.cheat();
+		
+		
+};
+
+let drawBoard = function() {
+        let context = this.canvas.getContext('2d');
+        const TILE_W = (w-1)/ Ex2.Logic.getCols();
+        const TILE_H = (h-1)/ Ex2.Logic.getRows();
+       var dist ;
+                
+        for(let i = 0; i < Ex2.Logic.getRows(); i++)
+            for(let j = 0; j < Ex2.Logic.getCols(); j++)
+			{
+				let val = Ex2.Logic.getCell(i, j); 
+				switch(val)
+				{
+					case 0 :
+						context.fillStyle = "gray";
+						break;
+					case 2:
+						context.fillStyle = "#eee4da";
+						dist=4;
+						break;
+					case 4 :
+						context.fillStyle = "#ede0c8";
+						dist=4
+						break;
+					
+					case 8:
+						context.fillStyle = "#f2b179";
+						dist=4;
+						break;
+					
+					case 16:
+						context.fillStyle = "#f59563";
+						dist = 9;
+						break;
+					
+					case 32:
+						context.fillStyle = "#f67c5f";
+						dist = 9;
+						break;
+					case 64:
+						context.fillStyle = "#f65e3b";
+						dist=9;
+						break;
+							
+					case 128:
+						context.fillStyle = "#edcf72";
+						dist = 13;
+						break;
+					
+					case 256:
+						context.fillStyle = "#edcc61";
+						dist = 13;
+						break;
+					
+					case 512:
+						context.fillStyle = "#edc850";
+						dist = 13;
+						break;
+					
+					case 1024:
+						context.fillStyle = "#edc53f";
+						dist = 19;
+						break;
+					
+					case 2048:
+						context.fillStyle = "#edc22e";
+						dist = 19;
+						break;
+					
+					
+					
+				}
+				context.fillRect(  j * TILE_W,  i * TILE_H, TILE_W , TILE_H);
+				context.strokeStyle ="#778899";
+				context.lineWidth = 10;
+				context.strokeRect( j * TILE_W,  i * TILE_H, TILE_W , TILE_H);
+				if(val!=0)
+				{
+					context.font = "20px italic bold";
+					context.fillStyle = "black";
+					context.fillText("" + val,  (j * TILE_W + TILE_W / 2)-dist ,  (i * TILE_H + TILE_H/2)+8);
+					
+				}
+			
+              
+            }
+    };
+
+
+
+  return {initModule,drawBoard};
+
+}();
---
+++ Ex2.Logic.js
@@ -0,0 +1,240 @@
+Ex2 = window.Ex2 || {};
+
+Ex2.Logic = function()
+{
+	let ROWS = 4;
+	let COLS = 4;
+	let SIZE =ROWS*COLS;
+	let tiles = [];
+	let over = false;
+	var ROTATIONS =
+	{
+		left:  0,
+		down:  1,
+		right: 2,
+		up:    3,
+		total: 4
+	};
+	
+	let initModule = function()
+	{
+		let a = parseInt(Math.random() * SIZE);//location of a
+		let b = a;
+		while(b===a)
+		{
+		  b = parseInt(Math.random() * SIZE); //location of b
+		}
+		for(var i = 0 ; i < SIZE ; i++)
+		{
+			tiles[i]=0;
+		}
+		tiles[a]=chooseStartNumber();;
+		tiles[b]=chooseStartNumber();;
+		
+	};
+	
+	let chooseStartNumber = function()// 2 or 4
+	{
+		var i = parseInt(Math.random()*2);
+		if(i===0)
+		{
+			return 2;
+		}
+		return 4;
+	};
+	
+	let getEmpties = function() //return the emptie tiles
+	{
+		var emptyTiles = [];
+		for(var i =0; i< SIZE ; i++)
+		{
+			if(tiles[i]===0)
+			{
+				emptyTiles.push(i);
+			}
+		}
+		return emptyTiles;
+	};
+	
+	let placeTile = function()//place a new tile in an empty one
+	{
+		var emptyTiles = getEmpties();
+		if(emptyTiles.length===0)
+		{
+			console.log("No free tiles");
+			console.log("check if we have tiles to merge");
+			if(nothingLeftToMerge())
+			{
+				console.log("over");
+				over = true;
+			}
+		}
+		else
+		{
+			var selectedTile = emptyTiles[Math.floor(Math.random() * emptyTiles.length)]
+			tiles[selectedTile] = chooseStartNumber();
+		}		
+	}
+	let nothingLeftToMerge = function() //check if there is no more merging to do
+	{
+		for(var i =0 ; i <SIZE ; i++)
+		{
+			if(tiles[i]===tiles[i+COLS])
+			{
+				console.log("check: "+ tiles[i]  +" = " + tiles[i+COLS] + " ? ")
+				return false;
+			}
+			if(!isAgainstWall(i+1))
+			{
+				if(tiles[i]===tiles[i+1])
+				{
+					console.log("check: "+ tiles[i]  +" = " + tiles[i+1] + " ? ")
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	let swap = function(i) //swap between two tiles
+	{
+		var currentIndex = i;
+		var currentTileValue = tiles[i];
+		var leftIndex = i - 1;
+		var leftTileValue = tiles[i - 1];
+		tiles[currentIndex] = leftTileValue;
+		tiles[leftIndex] = currentTileValue;
+	};
+	
+	
+	let checkForWin = function() //check if we have a tile with value of 2048 
+	{
+		for(let i =0; i<SIZE; i++)
+			if(tiles[i]===2048)
+			{
+				over =true;
+				Ex2.GUI.drawBoard();
+				setTimeout(function()
+				{
+					alert("YOU WIN!");
+				}, 200);
+			}
+		
+	};
+	let rotateTimes = function(number) //how many times to rotate the array
+	{
+		for (number; number > 0; number--)
+		{
+			rotate();
+		}
+	};
+	let rotate = function() //update the array
+	{
+		var updatedBoard = [];
+		for (var i = 0; i < COLS; i++){
+			updatedBoard = updatedBoard.concat(transpose(i).reverse());
+		}
+		tiles = updatedBoard;
+	};
+	let transpose = function(i)//change the array
+	{
+		 var column = [];
+		for (i; i < SIZE; i += COLS) 
+		{
+			column.push(tiles[i]);
+		}
+		return column;
+	};
+	let slideAll = function() //slide all tiles
+	{
+		for (var i = 0; i < SIZE; i++) 
+		{
+			if (!(tiles[i]===0))
+			{
+				slide(i);
+			}
+		}
+	};
+	let slide = function(index) //slide to left and do or swap or merge
+	{
+			for (var i = index; !isAgainstWall(i); i--)
+			{
+				if (tiles[i-1]===0)
+				{
+					swap(i);
+				} 
+				else if (tiles[i] === tiles[i - 1] )
+				{
+					merge(i);
+				}
+			}
+	};
+	let isAgainstWall = function(i)//if a tile is against the wall return true
+	{
+		return i%COLS===0;
+	}
+	let merge = function(i) //merge tile i with left tile
+	{
+		var currentIndex = i;
+		var leftIndex = i - 1;
+		var currentTileValue = tiles[i];
+		var leftTileValue = tiles[leftIndex];
+		tiles[leftIndex] = leftTileValue + currentTileValue;
+		tiles[i] = 0;
+
+	};
+	let cheat = function()// I added a cheat method if you press c you win 
+	{
+		if(!over)
+		{
+			var emptyTiles = getEmpties();
+			if(emptyTiles.length===0)
+			{
+				alert("NO MORE EMPTY TILES!!!");
+			}
+			else
+			{
+				var selectedTile = emptyTiles[Math.floor(Math.random() * emptyTiles.length)]
+				tiles[selectedTile] = 2048;
+				over = true;
+				Ex2.GUI.drawBoard();
+				setTimeout(function()
+				{
+					alert("YOU WIN!");
+				}, 200);
+		
+			}	
+		}		
+	}
+
+	let move = function(direction)//left, right , up down
+	{
+		
+		if(!over)
+		{
+			var rotationTimes =ROTATIONS[direction];
+			rotateTimes(rotationTimes);
+			slideAll();
+			placeTile();
+			rotateTimes((ROTATIONS['total'] - rotationTimes) % COLS);
+			if (over) 
+			{
+				alert('Game over!')
+			} 		
+			else 
+			{
+				checkForWin();
+			}
+			Ex2.GUI.drawBoard();/////////////////////////////////////
+
+		}
+	};
+	
+	let getRows = () => ROWS;
+    let getCols = () => COLS;
+    let getCell = (row, col) => tiles[toInd(row, col)];
+	 let toInd = (row, col) => row * COLS + col;
+	
+
+    return { initModule, getRows, getCols, getCell,move,placeTile,checkForWin,getEmpties,swap,rotate, rotateTimes,transpose,slideAll,slide,merge,cheat,nothingLeftToMerge}
+
+}();
